c82ce4d8e0f01a1854f4a00a67e43562885ef41c
piyushazanjad <piyusha_zanjad@csu.fullerton.edu>
2020-09-19 14:12:20 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAb2cfUil3DD4ovNgTF6wlVuhVRQ2JthLo",
+    authDomain: "clone-65ece.firebaseapp.com",
+    databaseURL: "https://clone-65ece.firebaseio.com",
+    projectId: "clone-65ece",
+    storageBucket: "clone-65ece.appspot.com",
+    messagingSenderId: "333011360324",
+    appId: "1:333011360324:web:125569b406c08f6eda7a11",
+    measurementId: "G-JRQSSC1X84"
+  };
